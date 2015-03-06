783baf051d2bd8c41bc5018c412b3594f12da130
Nathan Self <nathan@theselfs.org>
2015-03-06 16:41:17 -0500

    Initial commit

---
+++ board.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview
+ * This file represents a TicTacToe board object, with all needed drawing and
+ * state update functions.
+ */
+
+/**
+ * Creates an empty board object with no location
+ * @param {CanvasRenderingContext2D} context the 2D context of the canvas that
+ *     the board is drawn on.
+ * @constructor
+ */
+
+
+var card_files = [
+    "10_of_clubs.png",
+    "10_of_diamonds.png",
+    "10_of_hearts.png",
+    "10_of_spades.png",
+    "2_of_clubs.png",
+    "2_of_diamonds.png",
+    "2_of_hearts.png",
+    "2_of_spades.png",
+    "3_of_clubs.png",
+    "3_of_diamonds.png",
+    "3_of_hearts.png",
+    "3_of_spades.png",
+    "4_of_clubs.png",
+    "4_of_diamonds.png",
+    "4_of_hearts.png",
+    "4_of_spades.png",
+    "5_of_clubs.png",
+    "5_of_diamonds.png",
+    "5_of_hearts.png",
+    "5_of_spades.png",
+    "6_of_clubs.png",
+    "6_of_diamonds.png",
+    "6_of_hearts.png",
+    "6_of_spades.png",
+    "7_of_clubs.png",
+    "7_of_diamonds.png",
+    "7_of_hearts.png",
+    "7_of_spades.png",
+    "8_of_clubs.png",
+    "8_of_diamonds.png",
+    "8_of_hearts.png",
+    "8_of_spades.png",
+    "9_of_clubs.png",
+    "9_of_diamonds.png",
+    "9_of_hearts.png",
+    "9_of_spades.png",
+    "ace_of_clubs.png",
+    "ace_of_diamonds.png",
+    "ace_of_hearts.png",
+    "ace_of_spades2.png",
+    "jack_of_clubs2.png",
+    "jack_of_diamonds2.png",
+    "jack_of_hearts2.png",
+    "jack_of_spades2.png",
+    "king_of_clubs2.png",
+    "king_of_diamonds2.png",
+    "king_of_hearts2.png",
+    "king_of_spades2.png",
+    "queen_of_clubs2.png",
+    "queen_of_diamonds2.png",
+    "queen_of_hearts2.png",
+    "queen_of_spades2.png"];
+
+function board(context) {
+  this.mContext = context;
+
+  this.setup = function() {
+    console.log("SETTING UP BOARD");
+
+    var svg = d3.select('svg');
+    var avatar_g = svg.append('g');
+
+    avatar_g.append('svg:image')
+      .attr('x', 50)
+      .attr('y', 50)
+      .attr('width', 50)
+      .attr('height', 50)
+      .attr('xlink:href', 'images/penguin.png')
+  }
+
+}
